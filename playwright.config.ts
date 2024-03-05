85641219d8a39070f7a8fad5f6dbb34e63b660a7
Joram Schwartz <161503181+JoramSWS@users.noreply.github.com>
2024-03-05 15:06:05 -0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,18 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    [
-      "./node_modules/playwright-slack-report/dist/src/SlackReporter.js",
-      {
-        channels: ["test-test"], // provide one or more Slack channels
-        slackWebHookUrl: ${{ secrets.SLACK_WEBHOOK_URL }}
-        sendResults: "always", // "always" , "on-failure", "off"
-      },
-    ],
-    ["dot"], 
-    ['html'], // other reporters
-  ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
