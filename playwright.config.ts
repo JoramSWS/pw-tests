ba344cdc535bbc31e0d09ca78aa0dafbc367342f
JoramSWS <joram.s@secretweapon.studio>
2024-03-05 15:01:04 -0800

    revert to original config

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,25 +20,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    [
-      "./node_modules/playwright-slack-report/dist/src/SlackReporter.js",
-      {
-        channels: ["test-test"], // provide one or more Slack channels
-        slackWebHookUrl: "https://hooks.slack.com/services/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",
-        sendResults: "always", // "always" , "on-failure", "off"
-      },
-    ],
-    ["dot"], // other reporters
-  ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  //use: {
+  use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    //trace: 'on-first-retry',
-  //},
+    trace: 'on-first-retry',
+  },
 
   /* Configure projects for major browsers */
   projects: [
