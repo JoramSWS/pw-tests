e64bdbb0ac68af176f631fd13a8e369172536e0c
JoramSWS <joram.s@secretweapon.studio>
2024-03-05 11:27:21 -0800

    Restrict to Chrome

--- tests/PH-004-Talent-Edit-ShowMoreLess.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://secretweapon.studio/posthero/talent/');
-  await page.frameLocator('#miniextensions-iframe-embed-0OolpazVyjsidYGqtaaR iframe').locator('div:nth-child(5) > div > .ui > .css-veveu').first().click();
-  await page.frameLocator('#miniextensions-iframe-embed-0OolpazVyjsidYGqtaaR iframe').getByText('Show less').click();
-});
