b47945eb8a900e3a27db47c88195939513290505
C.J. Ware <cj@secretweapon.studio>
2024-05-03 12:53:37 -0700

    Committed

--- tests/PH-011-Login_NavSetup.spec.ts
+++ tests/PH-011-Login_NavSetup.spec.ts
@@ -9,7 +9,26 @@ test('test', async ({ page }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill(process.env.PASSWORD!);
   await page.getByRole('button', { name: 'Login' }).click();
- await page.frameLocator('#miniextensions-iframe-embed-01Omj0WJfpY5LNeAyTRQ iframe').getByRole('link', { name: 'PostHero Setup' }).click();
- await page.frameLocator('#miniextensions-iframe-embed-9cNb5KrWpIVH5FFL27Of iframe').getByText('Serial # 999999999999').click();
- await page.getByRole('link', { name: 'Logout' }).click();
+  await page.waitForSelector('text="Account"', { state: 'attached' });
+  
+  // test the page for warnings
+  let warningCount = await page.locator('text=Warning').count();
+  if (warningCount !== 0) {
+    console.log('Found warning on the Account page');
+  }
+  expect(warningCount).toBe(0);
+  
+
+  await page.frameLocator('#miniextensions-iframe-embed-01Omj0WJfpY5LNeAyTRQ iframe').getByRole('link', { name: 'PostHero Setup' }).click();
+  await page.waitForSelector('text="1. Your Cloud Store Drive"', { state: 'attached' });
+  
+  // test the page for warnings
+  warningCount = await page.locator('text=Warning').count();
+  if (warningCount !== 0) {
+    console.log('Found warning on the Setup page');
+  }
+  expect(warningCount).toBe(0);
+
+  await page.frameLocator('#miniextensions-iframe-embed-9cNb5KrWpIVH5FFL27Of iframe').getByText('Serial # 999999999999').click();
+  await page.getByRole('link', { name: 'Logout' }).click();
 });
