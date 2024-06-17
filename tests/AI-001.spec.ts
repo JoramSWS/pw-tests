d9715340b2980eef8132b7205643a41e5d199553
JoramSWS <joram.s@secretweapon.studio>
2024-06-17 13:39:23 -0700

    Commit

---
+++ tests/AI-001.spec.ts
@@ -0,0 +1,9 @@
+// Keep streamlit active for Crewhero AI; navigate to main app, mobile app, and embedded page
+
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://crewhero-mb.streamlit.app/');
+  await page.goto('https://crewhero.streamlit.app/');
+  await page.goto('https://secretweapon.studio/crewhero/beta/')
+});
