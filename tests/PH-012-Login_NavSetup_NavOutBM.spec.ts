d6a7ec93d7271d93fb13c1f45ea8115a70cf9770
C.J. Ware <cj@secretweapon.studio>
2024-10-18 10:15:26 -0700

    headless:false

--- tests/PH-012-Login_NavSetup_NavOutBM.spec.ts
+++ tests/PH-012-Login_NavSetup_NavOutBM.spec.ts
@@ -3,19 +3,17 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://secretweapon.tech/');
-  await page.locator('#nav_menu-2').getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'Login' }).click();
   await page.getByLabel('Email').click();
-  await page.getByLabel('Email').fill(process.env.USERNAME!);
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('demo@demo.com');
   await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill(process.env.PASSWORD!);
+  await page.getByLabel('Password').fill('demo_1234!');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.frameLocator('#miniextensions-iframe-embed-01Omj0WJfpY5LNeAyTRQ iframe').getByRole('link', { name: 'PostHero Setup' }).click();
-  // Listen for the popup (opens in new tab) event
-  const [popup] = await Promise.all([
-    page.waitForEvent('popup'),
-    // Trigger the action that opens the popup
-    await page.frameLocator("iframe[id^='miniextensions-iframe-embed-']").getByRole('link', { name: 'Cloud Store App' }).click()]);
-  await popup.waitForLoadState();
-  await popup.getByRole('link', { name: 'Products' }).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.frameLocator('#miniextensions-iframe-embed-YO9o58FXvFZIA35GCFFq iframe').getByRole('link', { name: 'Cloud Store App' }).click();
+  const page1 = await page1Promise;
+  await page1.getByRole('link', { name: 'Products' }).click();
   await page.getByRole('link', { name: 'Logout' }).click();
 });
